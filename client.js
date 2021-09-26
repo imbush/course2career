6aa30d169d560055b46c0829a5f0407570fb6af3
jpVinnie <jvr34@cornell.edu>
2021-09-26 07:37:25 -0400

    Final implementation

--- client.js
+++ client.js
@@ -1,30 +1,216 @@
+const card = (x, y, z) => {
+	return `
+	<div>
+		<div class="course-result">
+			<div class="micro-card-fill"></div>
+			<div class="basic-info">
+				<div>
+					<span class="card-sub1">${x}</span>
+					<span class="card-sub2">${y}</span>
+				</div>
+				<div>
+					<p class="card-name">${z}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	`
+}
+
+const detailCard = (v, w, x, y, z) =>  `<div class="details-result">
+											<div class="big-card-fill"></div>
+											<div class="basic-info">
+												<div>
+													<span class="card-sub1">`+ v + `</span>
+													<span class="card-sub2">` + w + `</span>
+												</div>
+												<div>
+													<p class="card-name">` + x + `</span>
+												</div>
+												<div class="description">
+													<p class="course-description">`
+														+ y +
+													`</p>
+													<div class="fill button">Add</div>
+													<a href="` + z + `" class="course-url" target="_blank">Read more 🡢</a>
+												</div>
+											</div>
+											</div>
+											`
+
 console.log('yup')
+console.log("We have loaded! WOOT!");
+
+myStorage = window.localStorage;
+
+const clearStorage = () => {
+	myStorage.clear();
+	onLoadUp()}
+
+	const onLoadUp = ()=> {	var arrayOfValues = [];
+							for(var i in myStorage){
+								if(myStorage.hasOwnProperty(i)){
+									arrayOfValues.push(myStorage[i].split(",SPLIT,"));
+								}
+							}
+
+							console.log(arrayOfValues);
+							console.log("Okay??")
+							const detailContainer = document.getElementById("details");
+							const resultsContainer = document.getElementById("added");
+
+							// resultsContainer.innerHTML="";
+
+							for(_class1=0; _class1<arrayOfValues.length; _class1++) {
+								_class=arrayOfValues[_class1]
+								console.log(_class[0])
+								console.log("Pretty pretty")
+								const newCard = card(_class[0], _class[1], _class[2]);
+								const newCardDetail = detailCard(_class[0], _class[1], _class[2], _class[3], _class[4]);
+
+								detailContainer.innerHTML = newCardDetail;
+								resultsContainer.innerHTML += newCard;
+							}
+						}
+	
+	onLoadUp();
+
+// const onLoadUpJobs = async()=> {
+// 	var arrayOfValues = [];
+// 	for(var i in myStorage){
+// 		if(myStorage.hasOwnProperty(i)){
+// 			arrayOfValues.push(myStorage[i].split(",SPLIT,"));
+// 		}
+// 	}
+// 	console.log("test")
+// 	console.log(arrayOfValues)
+
+// 	for(i=0; i<arrayOfValues.length; i++) {
+// 		_class=arrayOfValues[i]
+
+// 		// const dropDown = document.getElementById('dropDownId')
+// 		// const dropDownValue = dropDown[dropDown.value].value
+
+// 		const jobs = await fetch(`https://course2careerapi.herokuapp.com/api/jobs/Ithaca/${_class[0]}/${_class[1]}`, {
+// 				credentials: 'omit'		
+// 			}
+// 		)
+// 		.then(jobs => jobs.json())
+// 		console.log(JSON.stringify(jobs, null, 2))
+// 		const jobCard = card(jobs["title"], jobs["locations"], jobs["company"]);
+// 		const resultsContainer = document.getElementById("job-results");
+// 			resultsContainer.innerHTML += jobCard;
+// 	}
+// }
+
+// onLoadUpJobs();
 
 const getCourse = async () => {
+
 	const string = document.getElementById('courseSearchField').value
 	const strUp = string.toUpperCase()
 	const numIdx = strUp.search(/\d/)
 	const course = strUp.slice(0, numIdx)
 	const number = strUp.slice(numIdx)
 
-	const resp = await fetch(`http://course2careerapi.herokuapp.com/api/courses/${course}/${number}`)
-	.then(resp => resp.json())
-	return resp
+	try{ 
+		const resp = await fetch(`http://course2careerapi.herokuapp.com/api/courses/${course}/${number}`, {
+				credentials: 'omit'		
+			}
+		)
+		.then(resp => resp.json())
+		console.log(JSON.stringify(resp, null, 2))
+		
+		if (resp["error"] == "Course not found") {
+			alert("Invalid Course! Ex. CS2112")
+		}
+		else {
+			let classData = [resp["subject"], "SPLIT", resp["catalogNbr"], "SPLIT", resp["titleLong"], "SPLIT", resp["description"], "SPLIT", resp["rosterURL"]]
+			myStorage.setItem(resp["titleLong"], classData);
+			
+			console.log("Printing local storage:")
+			console.log(myStorage);
+
+			const newCard = card(resp["subject"], resp["catalogNbr"], resp["titleLong"]);
+
+			const newCardDetail = detailCard(resp["subject"], resp["catalogNbr"], resp["titleLong"], resp["description"], resp["rosterURL"])
+			const detailContainer = document.getElementById("details");
+			detailContainer.innerHTML = newCardDetail;
+			
+			const resultsContainer = document.getElementById("added");
+			resultsContainer.innerHTML += newCard;
+
+			return resp
+			
+		}
+	}
+	catch (error) {
+		alert("Invalid Course! Ex. CS2112")
+	}
 }
 
+const getJobs = async () => {
+// 	const course = strUp.slice(0, numIdx)
+// 	const number = strUp.slice(numIdx)
+
+	var arrayOfValues = [];
+	for(var i in myStorage){
+		if(myStorage.hasOwnProperty(i)){
+			arrayOfValues.push(myStorage[i].split(",SPLIT,"));
+		}
+	}
+	console.log("test")
+	console.log(arrayOfValues)
+
+	// for every class
+	for(i=0; i<arrayOfValues.length; i++) {
+		_class=arrayOfValues[i]
+		console.log("this is class")
+		console.log(_class)
+
+		// const dropDown = document.getElementById('dropDownId')
+		// const dropDownValue = dropDown[dropDown.value].value
+
+		const jobs = await fetch(`https://course2careerapi.herokuapp.com/api/jobs/Ithaca/${_class[0]}/${_class[1]}`, {
+				credentials: 'omit'		
+			}
+		)
+		.then(jobs => jobs.json())
+		console.log("this is string")
+		// console.log(JSON.stringify(jobs, null, 2))
+		
+		// going through jobs
+		for (i=0; jobs.length; i++) {
+			const jobs_=jobs[i]
+			const jobCard = card(jobs_["title"], jobs_["locations"], jobs_["company"]);
+			const bigJobCard= detailCard(jobs_["title"], jobs_["locations"], jobs_["company"], jobs_["description"], jobs_["url"])
+			const resultsContainer = document.getElementById("job-results");
+				resultsContainer.innerHTML += jobCard;
+				const detailContainer = document.getElementById("details");
+			detailContainer.innerHTML = bigJobCard;
+
+			console.log("Showing inner html of detail card");
+			console.log(detailContainer.innerHTML);
+		}
+		
+	}
+	return jobs
+}
+
+// document.getElementById("courseSearchField")
+//     .addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.key === 13) {
+//         document.getElementById("searchButton").click();
+//     }
+// });
+
 document.onload = () => {
+
 	const searchButton = document.getElementById('searchButton')
 	searchButton.addEventListener('click', () => {
+
 		console.log('I AM WORKING!')
 		getCourse(searchFieldValue)
 	})
 }
-/*Steps:
-
-1) get: course2careerapi.herokuapp.com/api/courses/<LETTERS>/<NUMBERS>
-
-2) get: course2careerapi.herokuapp.com/api/jobs/<location>/<[title+description Keywords]>
-
-3) get: /api/jobs/Ithaca/<subject>/<course_id>
-
-*/
--- domUpdates.js
+++ domUpdates.js
@@ -1,4 +1,6 @@
 const tag = document.createElement("div");
+const tag2 = document.createElement("div");
+const tag3 = document.createElement("div");
 
 let testName = "Objected-Oriented";
 let testSubject = "CS";
@@ -18,10 +20,39 @@ const createMiniCardTag = (courseName, subject, courseNo) =>  `<div class="cours
                                                                 </div>
                                                                 `
 
+const createBigCardTag = (courseName, subject, courseNo, courseDescription, courseRosterLink) =>  `<div class="details-result">
+                                                                                                    <div class="big-card-fill"></div>
+                                                                                                    <div class="basic-info">
+                                                                                                        <div>
+                                                                                                            <span class="card-sub1">`+ subject + `</span>
+                                                                                                            <span class="card-sub2">` + courseNo + `</span>
+                                                                                                        </div>
+                                                                                                        <div>
+                                                                                                            <p class="card-name">` + courseName + `</span>
+                                                                                                        </div>
+                                                                                                        <div class="description">
+                                                                                                            <p class="course-description">`
+                                                                                                                + courseDescription +
+                                                                                                            `</p>
+                                                                                                            <div class="fill button">Add</div>
+                                                                                                            <a href="` + courseRosterLink + `" class="course-url" target="_blank">Read more 🡢</a>
+                                                                                                        </div>
+                                                                                                    </div>
+                                                                                                    </div>
+                                                                                                    `
 
-tag.innerHTML = createMiniCardTag(testName, testSubject, testNo);
+// tag.innerHTML = createMiniCardTag("Poop", "DN", "21");
+// tag2.innerHTML = createMiniCardTag("Poop", "DN", "21");
+// tag3.innerHTML = createMiniCardTag("Poop", "DN", "21");
 
-const element = document.getElementById("course-results");
-element.appendChild(tag);
+// const element = document.getElementById("course-results");
+// element.appendChild(tag);
+// element.appendChild(tag2);
+// element.appendChild(tag3);
 
-console.log("Package delivered");
+// const bigCard = document.getElementById("details");
+// const newFatCard = createBigCardTag("Poop", "DN", "21", `Hello hello hello hellohello hello hellohello hello hellohello hello hellohello hello hellohello hello hellohello hello hellohello hello hello`, "https://www.youtube.com/watch?v=vFTnbvXraBo");
+
+// bigCard.innerHTML = newFatCard;
+
+// console.log("Package delivered");
